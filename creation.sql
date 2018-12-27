f31ef93e41d96a1de5ef4d4d40d0ef4aa4f9556a
Tushar Dhyani <dhyanitushar@gmail.com>
2018-12-27 15:48:47 +0530

    Supporting files

---
+++ creation.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE justdial;
+CREATE TABLE JustDial(
+	id bigint,
+	name varchar(100),
+	rating varchar(5),
+	address varchar(200),
+	phone varchar(12),
+	link varchar(1000),
+	category varchar(100),
+	city varchar(100)
+); 
