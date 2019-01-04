bba176e006fa78cf42bf89da85478a5d2edf1fa1
Tushar Dhyani <dhyanitushar@gmail.com>
2019-01-04 23:12:38 +0530

    Better parsing

--- script.py
+++ script.py
@@ -26,6 +26,7 @@ column=['id','name','rating','address','phone','link','category','city']
 
 
 def justdial():
+	#  Software data initialization
 	if not os.path.exists('logs'):
 		os.system('mkdir logs')
 	if not os.path.exists('./logs/links.csv'):
@@ -33,36 +34,47 @@ def justdial():
 		lnk.to_csv('./logs/links.csv', index=None)
 	if not os.path.exists('data'):
 		os.system('mkdir data')
+
+
 	lnk=pd.read_csv('./logs/links.csv')
 	l=list(lnk['links'])
+	# Looping through all the cities and categories
 	for city in cities:
 		df=pd.DataFrame(columns=column)
 		file_name=city+'.csv'
 		log_file=city+str(time.time())+'.txt'
+
 		if not os.path.exists('./data/'+file_name):
 			# df.to_csv('./data/'+file_name, index=None)
 			if 'linux' in platform.platform().lower():
 				os.system('cp ./data/'+file_name+' ./data/'+file_name.split('.')[0]+'_'+str(int(time.time()))+'_old.csv')
 			else:
 				os.system('copy ./data/'+file_name+' ./data/'+file_name.split('.')[0]+'_'+str(int(time.time()))+'_old.csv')
+		
 		df.to_csv('./data/'+file_name,index=None)
 		parent_link='https://www.justdial.com/'+city
+		
 		for category in categories:
 			link=str(parent_link+'/'+category).replace(' ','%20')
 			for page in range(1,100): # Change the range for overall site
 				t_link=link+'/page-'+str(page)
 				if t_link not in l:
-					# print(l)
+					print(t_link)
 					soup, page = soup_maker(t_link)
 					if (urllib.parse.urlparse(page.geturl()).query)=='':
 						try:
 							df=extracting_info_from_soup(soup, category, city)
-							df.to_csv('./data/'+file_name, mode='a', index=None, header=None)
+							df.to_csv('./data/'+file_name, 
+										mode='a', 
+										index=None, 
+										header=None
+									)
 							df.to_sql(name='justdial',
-								con=engine,
-								# schema='mysql',
-								if_exists='append',
-								index=False)
+										con=engine,
+										# schema='mysql',
+										if_exists='append',
+										index=False
+										)
 							print('\033[0;32m',t_link,' Done!\033[0;37m')
 							t_lnk=pd.DataFrame([t_link],columns=['links'])
 							t_lnk.to_csv('./logs/links.csv', index=None, mode='a', header=None)
@@ -71,9 +83,7 @@ def justdial():
 								log.write(str(time.time)+" Error occurred in {}".format(t_link))
 							print('\033[0;37mError in link: \033[1;30;41m',t_link,'\033[0;37m', e)
 					else:
-						# with open('categ.json', 'w') as outfile:
-						#     json.dump(categories[1:], outfile)
-						print('Breaks here')
+						print(category, 'Completed!')
 						break
 				else:
 					print('{} already completed!'.format(t_link))
