513619e5fcc394d62bcb8ae20a7b8379190c2cd1
Tushar Dhyani <dhyanitushar@gmail.com>
2019-01-04 23:12:09 +0530

    Updated for multiple numbers

--- creation.sql
+++ creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE justdial(
 	name varchar(100),
 	rating varchar(5),
 	address varchar(200),
-	phone varchar(12),
+	phone varchar(100),
 	link varchar(1000),
 	category varchar(100),
 	city varchar(100)
